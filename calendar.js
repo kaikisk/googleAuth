77d55675ba1895da66fa44f4beab6d5c819e25e6
kaikisk <kueobei@gmail.com>
2019-08-05 06:56:48 +0900

    commit

--- calendar.js
+++ calendar.js
@@ -1,4 +1,4 @@
-var clientId = '869215196237-728t4i6rfuptb5eaa53g3ddhgjsfkbgp.apps.googleusercontent.com';
+var clientId = '869215196237-v05klnhomptp0dqe02gbv0rcc5eejc3k.apps.googleusercontent.com';
 var apiKey = 'AIzaSyDxFCZD_khIcgWglksSDqNxK4fGbmtAHXc';
 
 // とりあえず、怖いので、カレンダーを読むだけの設定
@@ -6,48 +6,93 @@ var apiKey = 'AIzaSyDxFCZD_khIcgWglksSDqNxK4fGbmtAHXc';
 
 var scopes = ['https://www.googleapis.com/auth/calendar.readonly'];
 
-function handleClientLoad() {
-    // 予めAPI Consoleで設定したAPIキーを設定
-    gapi.client.setApiKey(apiKey);
-    console.log("successAA");
+// function handleClientLoad() {
+//     // 予めAPI Consoleで設定したAPIキーを設定
+//     gapi.client.setApiKey(apiKey);
+//     console.log("successAA");
 
-    // すでに認証済みかの確認をする。
+//     // すでに認証済みかの確認をする。
+//     window.setTimeout(checkAuth,1);
+// }
+
+// function checkAuth() {
+//     // immediateをtrueで指定することで、未認証の場合、ただちにエラーが返り、
+//     // handleAuthResultが呼び出される。
+//     console.log("successBB");
+//     gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+// }
+
+// function handleAuthResult(authResult) {
+//     console.log("success1");
+//     var authorizeButton = document.getElementById('authorize-button');
+//     console.log("success2");
+//     if (authResult && !authResult.error) {
+//         console.log("success3a");
+//         authorizeButton.style.visibility = 'hidden';
+//         makeApiCall();
+//     } else {
+//         console.log("success3b");
+//         authorizeButton.style.visibility = '';
+//         authorizeButton.onclick = handleAuthClick;
+//     }
+// }
+
+// function handleAuthClick(event) {
+//     // ここで、ポップアップ画面を表示して、OAuth認証を行う。
+//     gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+//     return false;
+// }
+
+// function makeApiCall() {
+//     var restRequest = gapi.client.request({
+//         'path': '/calendar/v3/users/me/calendarList'
+//     });
+//     restRequest.execute(function(calendarList) {
+//       console.dir(calendarList);
+//     });
+// }
+
+function handleClientLoad() {
+    gapi.client.setApiKey(apiKey);
     window.setTimeout(checkAuth,1);
+    checkAuth();
 }
 
 function checkAuth() {
-    // immediateをtrueで指定することで、未認証の場合、ただちにエラーが返り、
-    // handleAuthResultが呼び出される。
-    console.log("successBB");
-    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
+    handleAuthResult);
 }
 
 function handleAuthResult(authResult) {
-    console.log("success1");
     var authorizeButton = document.getElementById('authorize-button');
-    console.log("success2");
-    if (authResult && !authResult.error) {
-        console.log("success3a");
+    if (authResult) {
         authorizeButton.style.visibility = 'hidden';
         makeApiCall();
     } else {
-        console.log("success3b");
         authorizeButton.style.visibility = '';
         authorizeButton.onclick = handleAuthClick;
     }
 }
 
 function handleAuthClick(event) {
-    // ここで、ポップアップ画面を表示して、OAuth認証を行う。
-    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+    gapi.auth.authorize(
+    {client_id: clientId, scope: scopes, immediate: false},
+    handleAuthResult);
     return false;
 }
 
 function makeApiCall() {
-    var restRequest = gapi.client.request({
-        'path': '/calendar/v3/users/me/calendarList'
-    });
-    restRequest.execute(function(calendarList) {
-      console.dir(calendarList);
+    gapi.client.load('calendar', 'v3', function() {
+        var request = gapi.client.calendar.events.list({
+        'calendarId': 'primary'
+        });
+            
+        request.execute(function(resp) {
+        for (var i = 0; i < resp.items.length; i++) {
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(resp.items[i].summary));
+            document.getElementById('events').appendChild(li);
+        }
+        });
     });
 }
