4725d230f309917d368656769a340a5838088080
kaikis <kueobei@gmail.com>
2019-08-05 02:13:41 +0900

    commit

---
+++ calendar.js
@@ -0,0 +1,48 @@
+var clientId = '869215196237-728t4i6rfuptb5eaa53g3ddhgjsfkbgp.apps.googleusercontent.com';
+var apiKey = 'AIzaSyDxFCZD_khIcgWglksSDqNxK4fGbmtAHXc';
+
+// とりあえず、怖いので、カレンダーを読むだけの設定
+// https://www.googleapis.com/auth/plus.meは不要かも。。
+
+var scopes = ['https://www.googleapis.com/auth/plus.me',
+                   'https://www.googleapis.com/auth/calendar.readonly'];
+
+function handleClientLoad() {
+        // 予めAPI Consoleで設定したAPIキーを設定
+        gapi.client.setApiKey(apiKey);
+
+        // すでに認証済みかの確認をする。
+        window.setTimeout(checkAuth,1);
+}
+
+function checkAuth() {
+        // immediateをtrueで指定することで、未認証の場合、ただちにエラーが返り、
+        // handleAuthResultが呼び出される。
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+}
+
+function handleAuthResult(authResult) {
+        var authorizeButton = document.getElementById('authorize-button');
+        if (authResult && !authResult.error) {
+          authorizeButton.style.visibility = 'hidden';
+          makeApiCall();
+        } else {
+          authorizeButton.style.visibility = '';
+          authorizeButton.onclick = handleAuthClick;
+        }
+}
+
+function handleAuthClick(event) {
+        // ここで、ポップアップ画面を表示して、OAuth認証を行う。
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+        return false;
+}
+
+function makeApiCall() {
+    var restRequest = gapi.client.request({
+        'path': '/calendar/v3/users/me/calendarList'
+    });
+    restRequest.execute(function(calendarList) {
+      console.dir(calendarList);
+    });
+}
