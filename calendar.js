7762f08bce3b53a00c600037e073084606937643
kaikisk <kueobei@gmail.com>
2019-08-05 08:57:24 +0900

    commit

--- calendar.js
+++ calendar.js
@@ -120,7 +120,7 @@ function makeApiCall() {
             
         request.execute(function(resp) {
         for (var i = 0; i < resp.items.length; i++) {
-            console.log(i+"番目: "+resp.item[i+1]);
+            console.log(i+"番目: "+resp);
             var li = document.createElement('li');
             li.appendChild(document.createTextNode(resp.items[i].summary));
             document.getElementById('events').appendChild(li);
